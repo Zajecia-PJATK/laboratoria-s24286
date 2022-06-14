602903e23e17d37cabccbba8e43589bf4fa5173c
s24286 <93827106+s24286@users.noreply.github.com>
2022-06-14 18:36:45 +0200

    Create zad4.java

---
+++ Laboratorium_7/zad4.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj nazwe pierwszego pliku:");
+        String firstFileName = scanner.next();
+        scanner.nextLine();
+        System.out.print("Podaj nazwe drugiego pliku:");
+        String secondFileName = scanner.next();
+
+
+
+        try {
+            PrintWriter firstFile = new PrintWriter(new FileWriter(firstFileName, true));
+            File secondFile = new File(secondFileName);
+
+            Scanner secondFileData = new Scanner(secondFile);
+            while (secondFileData.hasNext()) {
+                firstFile.println(secondFileData.nextLine());
+            }
+            firstFile.close();
+            secondFileData.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Problem z otwarciem drugiego pliku.");
+        } catch (IOException e) {
+            System.out.println("Problem z otwarciem pierwszego pliku.");
+        }
+
+    }
+}
