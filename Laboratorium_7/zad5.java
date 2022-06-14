1d6a77f681bac8dd081dbff4f242ca9b2da7ec89
s24286 <93827106+s24286@users.noreply.github.com>
2022-06-14 18:54:28 +0200

    Create zad5.java

---
+++ Laboratorium_7/zad5.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        File file = new File("plik.txt");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj liczbę linii do odczytu:");
+        int linesToRead = scanner.nextInt();
+        ArrayList<String> lines = new ArrayList<>();
+
+        try {
+            Scanner fileData = new Scanner(file);
+            while (fileData.hasNext()) {
+                lines.add(fileData.nextLine());
+            }
+
+            for (int i = lines.size() - linesToRead; i < lines.size(); i++) {
+                if (i < 0) i = 0;
+                System.out.println(lines.get(i));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Problem z otwarciem pliku!");
+        }
+    }
+}
