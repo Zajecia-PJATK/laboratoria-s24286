31b3a9a0a233e8f409fd57aeb3a9d8bf399534fb
s24286 <93827106+s24286@users.noreply.github.com>
2022-06-14 19:36:49 +0200

    Create zad7.java

---
+++ Laboratorium_7/zad7.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj nazwę pliku:");
+        String fileName = scanner.next();
+
+        try {
+            long bytes = Files.size(Paths.get(fileName));
+            System.out.println("Rozmiar pliku wynosi: " + bytes + " bajtów");
+        } catch (IOException e) {
+            System.out.println("Problem z otwarciem pliku.");
+        }
+    }
+}
